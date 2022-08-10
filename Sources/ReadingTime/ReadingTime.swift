5b161f25e29348d9231ab9b3e61a652627a16862
Pol Piella Abadia <hi@polpiella.dev>
2022-08-10 13:37:54 +0200

    Refactors the API
    - Moves API to its own file and out of tests
    - Removes `@testable` from `ReadingTime`’s import statement in tests

--- Sources/ReadingTime/ReadingTime.swift
+++ Sources/ReadingTime/ReadingTime.swift
@@ -1,1 +1,13 @@
+import Foundation
 
+public enum ReadingTime {
+    public static func calculate(for content: String, wpm: Int = 265) -> TimeInterval {
+        let chararacterSet = CharacterSet.whitespacesAndNewlines.union(.punctuationCharacters)
+        let components = content.components(separatedBy: chararacterSet)
+        let words = components.filter { !$0.isEmpty }
+        let timeIntervalInMinutes = Double(words.count) / Double(wpm)
+        let timeIntervalInMilliseconds = timeIntervalInMinutes * 60 * 1000
+
+        return round(timeIntervalInMilliseconds * 100) / 100.0
+    }
+}
--- Tests/ReadingTimeTests/ReadingTimeTests.swift
+++ Tests/ReadingTimeTests/ReadingTimeTests.swift
@@ -1,17 +1,5 @@
 import XCTest
-@testable import ReadingTime
-
-enum ReadingTime {
-    static func calculate(for content: String, wpm: Int = 265) -> TimeInterval {
-        let chararacterSet = CharacterSet.whitespacesAndNewlines.union(.punctuationCharacters)
-        let components = content.components(separatedBy: chararacterSet)
-        let words = components.filter { !$0.isEmpty }
-        let timeIntervalInMinutes = Double(words.count) / Double(wpm)
-        let timeIntervalInMilliseconds = timeIntervalInMinutes * 60 * 1000
-
-        return round(timeIntervalInMilliseconds * 100) / 100.0
-    }
-}
+import ReadingTime
 
 final class ReadingTimeTests: XCTestCase {
     func test_GivenDefaultWPMIsUsed_WhenContentsOfAFileAreProvider_ThenReadingTimeIsReturned() {
