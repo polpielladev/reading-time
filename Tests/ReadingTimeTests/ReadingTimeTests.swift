486c08dfa0dc40d0ab0407a84be8ecb7c0e3ed8f
Pol Piella Abadia <hi@polpiella.dev>
2022-08-10 13:59:23 +0200

    Adds new test for custom `wpm` number

--- Tests/ReadingTimeTests/ReadingTimeTests.swift
+++ Tests/ReadingTimeTests/ReadingTimeTests.swift
@@ -27,4 +27,16 @@ final class ReadingTimeTests: XCTestCase {
         // Clean up
         try FileManager.default.removeItem(at: testFileURL)
     }
+    
+    func test_GivenCustomWPMIsUsed_WhenFileURLIsProvided_ThenReadingTimeIsReturned() throws {
+        let testFileURL = FileManager.default.temporaryDirectory.appendingPathComponent("test.md")
+        try "Hello World! This is my article!".data(using: .utf8)!.write(to: testFileURL)
+        
+        let calculatedTime = ReadingTime.calculate(for: testFileURL, wpm: 1)
+        
+        XCTAssertEqual(calculatedTime, 360000.0)
+
+        // Clean up
+        try FileManager.default.removeItem(at: testFileURL)
+    }
 }
