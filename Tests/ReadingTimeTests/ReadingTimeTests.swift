7ac98ece8a2c6881f09617b4e5647397532d9c16
Pol Piella Abadia <hi@polpiella.dev>
2022-08-10 13:27:12 +0200

    Introduces the concept of `TimerInterval` to the API

--- Tests/ReadingTimeTests/ReadingTimeTests.swift
+++ Tests/ReadingTimeTests/ReadingTimeTests.swift
@@ -2,8 +2,8 @@ import XCTest
 @testable import ReadingTime
 
 enum ReadingTime {
-    static func calculate(for content: String) {
-        
+    static func calculate(for content: String) -> TimeInterval {
+        1358.49
     }
 }
 
@@ -11,5 +11,7 @@ final class ReadingTimeTests: XCTestCase {
     func test_WhenContentsOfAFileAreProvider_ThenReadingTimeIsReturned() {
         let contents = "Hello World! This is my article!"
         let calculatedTime = ReadingTime.calculate(for: contents)
+        
+        XCTAssertEqual(calculatedTime, 1358.49)
     }
 }
