bcf304bc18718faac13fc116cfdd9d53b77d6b85
Pol Piella Abadia <hi@polpiella.dev>
2022-08-10 13:35:11 +0200

    Adds `wpm` parameter to `calculate` call

--- Tests/ReadingTimeTests/ReadingTimeTests.swift
+++ Tests/ReadingTimeTests/ReadingTimeTests.swift
@@ -2,11 +2,11 @@ import XCTest
 @testable import ReadingTime
 
 enum ReadingTime {
-    static func calculate(for content: String) -> TimeInterval {
+    static func calculate(for content: String, wpm: Int = 265) -> TimeInterval {
         let chararacterSet = CharacterSet.whitespacesAndNewlines.union(.punctuationCharacters)
         let components = content.components(separatedBy: chararacterSet)
         let words = components.filter { !$0.isEmpty }
-        let timeIntervalInMinutes = Double(words.count) / 265.0
+        let timeIntervalInMinutes = Double(words.count) / Double(wpm)
         let timeIntervalInMilliseconds = timeIntervalInMinutes * 60 * 1000
 
         return round(timeIntervalInMilliseconds * 100) / 100.0
@@ -14,10 +14,15 @@ enum ReadingTime {
 }
 
 final class ReadingTimeTests: XCTestCase {
-    func test_WhenContentsOfAFileAreProvider_ThenReadingTimeIsReturned() {
+    func test_GivenDefaultWPMIsUsed_WhenContentsOfAFileAreProvider_ThenReadingTimeIsReturned() {
         let contents = "Hello World! This is my article!"
         let calculatedTime = ReadingTime.calculate(for: contents)
         
         XCTAssertEqual(calculatedTime, 1358.49)
+    }
+    
+    func test_GivenCustomWPMIsUsed_WhenContentsOfAFileAreProvider_ThenReadingTimeIsReturned() {
+        let contents = "Hello World! This is my article!"
+        let calculatedTime = ReadingTime.calculate(for: contents, wpm: 1)
     }
 }
