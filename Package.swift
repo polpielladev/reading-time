9963b5e0b12220a130f39278f5f0a0c927a836dd
Pol Piella Abadia <hi@polpiella.dev>
2022-09-23 18:55:29 +0100

    Adds new executable target for AWS lambda

--- Package.swift
+++ Package.swift
@@ -1,13 +1,10 @@
 // swift-tools-version: 5.6
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "ReadingTime",
     platforms: [.iOS(.v8), .macOS(.v10_13)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ReadingTime",
             targets: ["ReadingTime"]),
@@ -16,8 +13,6 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-markdown.git", branch: "main")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ReadingTime",
             dependencies: [.product(name: "Markdown", package: "swift-markdown")]),
@@ -26,5 +21,6 @@ let package = Package(
             dependencies: ["ReadingTime"],
             resources: [.copy("MockData")]
         ),
+        .executableTarget(name: "ReadingTimeLambda", dependencies: [])
     ]
 )
