c068f62f22f0a75a8165441553d2903b2e89cf83
Pol Piella Abadia <hi@polpiella.dev>
2022-09-24 16:03:17 +0100

    Creates a HTML template with a form that can actually be used

--- Sources/ReadingTimeSite/routes.swift
+++ Sources/ReadingTimeSite/routes.swift
@@ -1,11 +1,47 @@
 import Vapor
 import Plot
+import ReadingTime
+
+let dateComponentsFormatter: DateComponentsFormatter = {
+    var formatter = DateComponentsFormatter()
+    formatter.allowedUnits = [.minute, .second]
+    formatter.unitsStyle = .abbreviated
+    return formatter
+}()
 
 func routes(_ app: Application) throws {
     app.get { request async throws -> HTML in
-        HTML(
+        let content = try? request.query.get(String.self, at: "content")
+        var readingTime: String? = nil
+        if let content = content {
+            let calculation = ReadingTime.calculate(for: content)
+            readingTime = dateComponentsFormatter.string(from: calculation)
+        }
+        
+        return HTML(
+            .head(
+                .title("Reading Time Playground!"),
+                .stylesheet("https://unpkg.com/@picocss/pico@latest/css/pico.min.css"),
+                .stylesheet("index.css")
+            ),
             .body(
-                .h1("Hello World")
+                .div(
+                    .class("app"),
+                    .h1(
+                        "Reading Time Playground!"
+                    ),
+                    .form(
+                        .id("playground"),
+                        .textarea(
+                            "\(content ?? "")",
+                            .placeholder("Enter your markdown here!"),
+                            .name("content"),
+                            .attribute(named: "form", value: "playground")
+                        ),
+                        .if(readingTime != nil, .h3("Estimated Reading Time: \(readingTime ?? "")")),
+                        .button(.type(.submit), "Calculate Reading Time!")
+                    )
+                )
             )
         )
     }
