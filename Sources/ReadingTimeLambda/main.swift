976de6411501639336e7b178987a7caa3d73104e
Pol Piella Abadia <hi@polpiella.dev>
2022-09-26 20:24:43 +0100

    Causes another swiftlint issue

--- Sources/ReadingTimeLambda/main.swift
+++ Sources/ReadingTimeLambda/main.swift
@@ -13,7 +13,7 @@ private struct Response: Codable {
 
 extension JSONEncoder {
     func encodeAsString<T: Encodable>(_ value: T) throws -> String {
-        try String(decoding: self.encode(value), as: Unicode.UTF8.self)
+        try! String(decoding: self.encode(value), as: Unicode.UTF8.self)
     }
 }
 
