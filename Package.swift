78a62d07abbb3098e8ba3cd83f4666fd8b4e8e04
Pol Piella Abadia <hi@polpiella.dev>
2022-10-02 18:55:30 +0100

    ci(gha): removes all concept of `Danger` whenever the `os` is `Windows`

--- Package.swift
+++ Package.swift
@@ -9,59 +9,60 @@ var dependencies: [Package.Dependency] = [
     .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.4")
 ]
 
+var targets: [Target] = [
+    .target(
+        name: "ReadingTime",
+        dependencies: [.product(name: "Markdown", package: "swift-markdown")]),
+    .testTarget(
+        name: "ReadingTimeTests",
+        dependencies: ["ReadingTime"],
+        resources: [.copy("MockData")]
+    ),
+    .executableTarget(name: "ReadingTimeLambda", dependencies: [
+        .product(name: "AWSLambdaRuntime", package: "swift-aws-lambda-runtime"),
+        .product(name: "AWSLambdaEvents", package: "swift-aws-lambda-runtime"),
+        "ReadingTime"
+    ], exclude: ["Scripts"]),
+    .target(
+        name: "ReadingTimeSite",
+        dependencies: [
+            .product(name: "Vapor", package: "vapor"),
+            .product(name: "Plot", package: "plot"),
+            "ReadingTime"
+        ],
+        swiftSettings: [
+            .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
+        ]
+    ),
+    .executableTarget(
+        name: "ReadingTimeSiteRunner",
+        dependencies: ["ReadingTimeSite"]
+    ),
+    .executableTarget(
+        name: "ReadingTimeCLI",
+        dependencies: ["ReadingTime", .product(name: "ArgumentParser", package: "swift-argument-parser")]
+    )
+]
+
+var products: [Product] = [
+    .library(
+        name: "ReadingTime",
+        targets: ["ReadingTime"]),
+    .executable(
+        name: "ReadingTimeCLI",
+        targets: ["ReadingTimeCLI"])
+]
+
 #if !os(Windows)
 dependencies.append(.package(url: "https://github.com/danger/swift.git", from: "3.12.1"))
+targets.append(.target(name: "DangerDeps", dependencies: [.product(name: "Danger", package: "swift")]))
+products.append(.library(name: "DangerDeps", type: .dynamic, targets: ["DangerDeps"]))
 #endif
 
 let package = Package(
     name: "ReadingTime",
     platforms: [.iOS(.v8), .macOS(.v12)],
-    products: [
-        .library(
-            name: "ReadingTime",
-            targets: ["ReadingTime"]),
-        .library(
-            name: "DangerDeps",
-            type: .dynamic,
-            targets: ["DangerDeps"]),
-        .executable(
-            name: "ReadingTimeCLI",
-            targets: ["ReadingTimeCLI"])
-    ],
+    products: products,
     dependencies: dependencies,
-    targets: [
-        .target(
-            name: "ReadingTime",
-            dependencies: [.product(name: "Markdown", package: "swift-markdown")]),
-        .testTarget(
-            name: "ReadingTimeTests",
-            dependencies: ["ReadingTime"],
-            resources: [.copy("MockData")]
-        ),
-        .target(name: "DangerDeps", dependencies: [.product(name: "Danger", package: "swift", condition: .when(platforms: [.macOS, .linux]))]),
-        .executableTarget(name: "ReadingTimeLambda", dependencies: [
-            .product(name: "AWSLambdaRuntime", package: "swift-aws-lambda-runtime"),
-            .product(name: "AWSLambdaEvents", package: "swift-aws-lambda-runtime"),
-            "ReadingTime"
-        ], exclude: ["Scripts"]),
-        .target(
-            name: "ReadingTimeSite",
-            dependencies: [
-                .product(name: "Vapor", package: "vapor"),
-                .product(name: "Plot", package: "plot"),
-                "ReadingTime"
-            ],
-            swiftSettings: [
-                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
-            ]
-        ),
-        .executableTarget(
-            name: "ReadingTimeSiteRunner",
-            dependencies: ["ReadingTimeSite"]
-        ),
-        .executableTarget(
-            name: "ReadingTimeCLI",
-            dependencies: ["ReadingTime", .product(name: "ArgumentParser", package: "swift-argument-parser")]
-        )
-    ]
+    targets: targets
 )
