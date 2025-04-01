61fe62af71e147560d59cc1428d604dedd5af051
Pol Piella Abadia <hi@polpiella.dev>
2025-04-01 19:56:08 +0100

    Removes all unnecessary platforms

--- Package.swift
+++ Package.swift
@@ -1,69 +1,33 @@
 // swift-tools-version: 5.6
 import PackageDescription
 
-var dependencies: [Package.Dependency] = [
-    .package(url: "https://github.com/apple/swift-markdown.git", branch: "main"),
-    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.4")
-]
-
-var targets: [Target] = [
-    .target(
-        name: "ReadingTime",
-        dependencies: [.product(name: "Markdown", package: "swift-markdown")]),
-    .testTarget(
-        name: "ReadingTimeTests",
-        dependencies: ["ReadingTime"],
-        resources: [.copy("MockData")]
-    ),
-    .executableTarget(
-        name: "ReadingTimeCLI",
-        dependencies: ["ReadingTime", .product(name: "ArgumentParser", package: "swift-argument-parser")]
-    )
-]
-
-#if !os(Windows)
-targets.append(contentsOf: [
-    .executableTarget(name: "ReadingTimeLambda", dependencies: [
-        .product(name: "AWSLambdaRuntime", package: "swift-aws-lambda-runtime"),
-        .product(name: "AWSLambdaEvents", package: "swift-aws-lambda-runtime"),
-        "ReadingTime"
-    ], exclude: ["Scripts"]),
-    .target(
-        name: "ReadingTimeSite",
-        dependencies: [
-            .product(name: "Vapor", package: "vapor"),
-            .product(name: "Plot", package: "plot"),
-            "ReadingTime"
-        ],
-        swiftSettings: [
-            .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
-        ]
-    ),
-    .executableTarget(
-        name: "ReadingTimeSiteRunner",
-        dependencies: ["ReadingTimeSite"]
-    ),
-])
-dependencies.append(contentsOf: [
-    .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", from: "0.1.0"),
-    .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-    .package(url: "https://github.com/JohnSundell/Plot.git", from: "0.5.0"),
-])
-#endif
-
-var products: [Product] = [
-    .library(
-        name: "ReadingTime",
-        targets: ["ReadingTime"]),
-    .executable(
-        name: "ReadingTimeCLI",
-        targets: ["ReadingTimeCLI"])
-]
-
 let package = Package(
     name: "ReadingTime",
     platforms: [.iOS(.v15), .macOS(.v12), .macCatalyst(.v15)],
-    products: products,
-    dependencies: dependencies,
-    targets: targets
+    products: [
+        .library(
+            name: "ReadingTime",
+            targets: ["ReadingTime"]),
+        .executable(
+            name: "ReadingTimeCLI",
+            targets: ["ReadingTimeCLI"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-markdown.git", branch: "main"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.4")
+    ],
+    targets: [
+        .target(
+            name: "ReadingTime",
+            dependencies: [.product(name: "Markdown", package: "swift-markdown")]),
+        .testTarget(
+            name: "ReadingTimeTests",
+            dependencies: ["ReadingTime"],
+            resources: [.copy("MockData")]
+        ),
+        .executableTarget(
+            name: "ReadingTimeCLI",
+            dependencies: ["ReadingTime", .product(name: "ArgumentParser", package: "swift-argument-parser")]
+        )
+    ]
 )
